afa7692b2f45904e43b8bc68fa63f5b4c7d7d225
VPN-V2Ray <46552880+VPN-V2Ray@users.noreply.github.com>
2019-06-02 12:30:04 +0800

    Add files via upload

---
+++ Win.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+COL='\033[1;33m'
+NC='\033[0m'
+
+sudo git clone https://github.com/VPN-V2Ray/WindscribeVPN
+
+cd WindscribeVPN
+sudo bash install.sh
+
+cd ..
+sudo rm -r ./WindscribeVPN
+
+exec bash
